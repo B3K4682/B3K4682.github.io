9d8d87e5bb49799c636dd3adbfed68f9f3dc9abb
Parsa-29 <74831323+Parsa-29@users.noreply.github.com>
2022-06-10 18:21:43 +0400

    award & offer section

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,22 @@ $('#navigation-close-btn').on('click', () => {
 $(".top-picker-item").click(function(){
     $(this).toggleClass('picker-active');
 });
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 4,
+        loop: true,
+        margin: 10,
+        // autoplay: true,
+        // autoWidth: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        responsiveClass: true,
+        responsive: {
+            955: {
+                items: 4,
+            },
+        }
+    });
+
+});
