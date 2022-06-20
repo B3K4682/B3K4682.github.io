92d8e9d7992106b58713f9e58a63e8077c114a8c
Parsa-29 <74831323+Parsa-29@users.noreply.github.com>
2022-06-20 15:56:46 +0400

    final

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 $('#burger-btn').on('click', () => {
     $('.navigation-body').css('right', '0%');
     $('#body').css('overflow', 'hidden');
+    console.log("zd")
 });
 
 $('#navigation-close-btn').on('click', () => {
