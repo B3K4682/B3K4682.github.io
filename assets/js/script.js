d7b4215dfea5c9d39981bc3f42b28923b30628f9
LashaBidzina <80463840+LashaBidzina@users.noreply.github.com>
2022-06-30 15:31:15 +0400

    slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,34 @@
-$('#burger-btn').on('click', () => {
-    $('.navigation-body').css('right', '0%');
-    $('#body').css('overflow', 'hidden');
-    console.log("zd")
+$("#burger-btn").on("click", () => {
+    $(".navigation-body").css("right", "0%");
+    $("#body").css("overflow", "hidden");
+    console.log("zd");
 });
 
-$('#navigation-close-btn').on('click', () => {
-    $('.navigation-body').css('right', '100%');
-    $('#body').css('overflow', 'auto');
+$("#navigation-close-btn").on("click", () => {
+    $(".navigation-body").css("right", "100%");
+    $("#body").css("overflow", "auto");
 });
 
 $(".top-picker-item").click(function () {
-    $(this).toggleClass('picker-active');
+    $(this).toggleClass("picker-active");
 });
 
-document.querySelector('.browse-btn').onmousemove = (e) => {
+document.querySelector(".browse-btn").onmousemove = (e) => {
+    const x = e.pageX - e.target.offsetLeft;
+    const y = e.pageY - e.target.offsetTop;
 
-    const x = e.pageX - e.target.offsetLeft
-    const y = e.pageY - e.target.offsetTop
-
-    e.target.style.setProperty('--x', `${x}px`)
-    e.target.style.setProperty('--y', `${y}px`)
-
-}
+    e.target.style.setProperty("--x", `${x}px`);
+    e.target.style.setProperty("--y", `${y}px`);
+};
 
 const button = document.querySelector(".footer-account");
 
 const readout = document.querySelector("p");
 
 button.addEventListener("mousemove", (e) => {
-  const { x, y } = button.getBoundingClientRect();
-  button.style.setProperty("--x", e.clientX - x);
-  button.style.setProperty("--y", e.clientY - y);
+    const { x, y } = button.getBoundingClientRect();
+    button.style.setProperty("--x", e.clientX - x);
+    button.style.setProperty("--y", e.clientY - y);
 });
 
 $(document).ready(function () {
@@ -49,30 +47,30 @@ $(document).ready(function () {
             },
 
             770: {
-                items: 3
+                items: 3,
             },
 
             500: {
-                items: 2
+                items: 2,
             },
 
             0: {
-                items: 1
-            }
-        }
+                items: 1,
+            },
+        },
     });
 });
 
-$('.destination-items').slick({
+$(".destination-items").slick({
     dots: false,
     arrows: false,
     centerMode: true,
     slidesToShow: 3,
     slidesToScroll: 2,
-    variableWidth: true
+    variableWidth: true,
 });
 
-$('.adventure-items').slick({
+$(".adventure-items").slick({
     dots: false,
     arrows: true,
     centerMode: true,
@@ -84,13 +82,13 @@ $('.adventure-items').slick({
             breakpoint: 676,
             settings: {
                 arrows: false,
-            }
-        }
-    ]
+            },
+        },
+    ],
 });
 
 if ($(window).width() < 640) {
-    $('.type-items').slick({
+    $(".type-items").slick({
         dots: false,
         arrows: false,
         variableWidth: true,
@@ -100,7 +98,7 @@ if ($(window).width() < 640) {
 }
 
 if ($(window).width() < 760) {
-    $('.stay-items').slick({
+    $(".stay-items").slick({
         dots: false,
         arrows: false,
         variableWidth: true,
@@ -111,27 +109,27 @@ if ($(window).width() < 760) {
                 breakpoint: 401,
                 settings: {
                     variableWidth: false,
-                }
-            }
-        ]
+                },
+            },
+        ],
     });
 }
 
-$('.book-img').slick({
+$(".book-img").slick({
     dots: true,
     arrows: false,
     slidesToShow: 1,
     slidesToScroll: 1,
 });
 
-$('.stay-img').slick({
+$(".stay-img").slick({
     dots: true,
     arrows: true,
     slidesToShow: 1,
     slidesToScroll: 1,
 });
 
-$('.comments-items').slick({
+$(".comments-items").slick({
     loop: false,
     infinite: false,
     dots: false,
@@ -139,14 +137,15 @@ $('.comments-items').slick({
     slidesToShow: 3,
     slidesToScroll: 3,
     variableWidth: true,
-    prevArrow: $('.slick-prev'),
-    nextArrow: $('.slick-next'),
+    prevArrow: $(".slick-prev"),
+    nextArrow: $(".slick-next"),
     responsive: [
         {
             breakpoint: 676,
             settings: {
                 centerMode: true,
-            }
-        }
-    ]
+            },
+        },
+    ],
 });
+
