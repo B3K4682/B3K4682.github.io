c90f832f5869d2061edb3d975811b9c8cad7d46c
Parsa-29 <74831323+Parsa-29@users.noreply.github.com>
2022-07-03 15:31:40 +0400

    .

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,22 +1,22 @@
-$(".products-slider").slick({
+$(".slider-items").slick({
     slidesToShow: 3,
     slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 10000,
+    // autoplay: true,
+    // autoplaySpeed: 10000,
     variableWidth: true,
     dots: true,
     arrows: true,
     centerMode: true,
-    prevArrow: $(".slick-prev"),
-    nextArrow: $(".slick-next"),
+    prevArrow: $(".product-prev"),
+    nextArrow: $(".product-next"),
     responsive: [
         {
             breakpoint: 768,
             settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1,
-                variableWidth: false,
-                centerMode: false,
+                // variableWidth: false,
+                centerMode: true,
             },
         },
     ],
