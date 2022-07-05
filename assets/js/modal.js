4e3fccfe2847f5fbc1ea57c55a9b17fa8bc47a33
Parsa-29 <74831323+Parsa-29@users.noreply.github.com>
2022-07-05 18:45:09 +0400

    .

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,7 +1,16 @@
-$(".close").click( () => {
+$(".close").click(() => {
     $(".modal-vv").hide();
 });
 
-$(".join_btn").click( () => {
+$(".join_btn").click(() => {
     $("#signup_modal").css("display", "flex");
 })
+
+$('.pass_visible_btn').click(() => {
+    var passInput = $('.password_input');
+    if (passInput.attr('type') === 'password') {
+        passInput.attr('type', 'text');
+    } else {
+        passInput.attr('type', 'password');
+    }
+});
