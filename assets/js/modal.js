da817adf0b0159b81f510875f91a293810047098
LashaBidzina <80463840+LashaBidzina@users.noreply.github.com>
2022-07-11 22:52:32 +0400

    ,

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,21 +1,23 @@
-$(".modal-vv").each((index, modal) => {
-    $(modal).find('.close').click(() => {
-        $(modal).hide();
-    })
-})
+$("[data-modal]").each((index, modal) => {
+    $(modal)
+        .find(".close")
+        .click(() => {
+            $(modal).hide();
+        });
+});
 
 $("[data-toggle-modal]").each((index, button) => {
-    let modalId = $(button).attr('data-toggle-modal');
-    $(button).on('click', () => {
-        $('#'+modalId).css("display", "flex");
-    })
-})
+    let modalId = $(button).attr("data-toggle-modal");
+    $(button).on("click", () => {
+        $("#" + modalId).css("display", "flex");
+    });
+});
 
-$('.pass_visible_btn').click(() => {
-    var passInput = $('.password_input');
-    if (passInput.attr('type') === 'password') {
-        passInput.attr('type', 'text');
+$(".pass_visible_btn").click(() => {
+    var passInput = $(".password_input");
+    if (passInput.attr("type") === "password") {
+        passInput.attr("type", "text");
     } else {
-        passInput.attr('type', 'password');
+        passInput.attr("type", "password");
     }
 });
